4d78ad61ea4b6f8f17a4def1ff4a410a24152251
Thonny Dev <149270844+Thonny-Developer@users.noreply.github.com>
2024-12-04 09:29:11 -0500

    Create router.lua

---
+++ router.lua
@@ -0,0 +1,12 @@
+--[[
+ 
+    Thonny Hub v1.2
+    by thonny_dev
+ 
+]]--
+ 
+if game.PlaceId == 13822889 then 
+    loadstring(game:HttpGet( "https://pastebin.com/raw/JeLCEpwp" ))("") --// Lumber Tycoon 2 //--
+elseif game.PlaceId == 537413528 then
+    loadstring(game:HttpGet( "https://pastebin.com/raw/g05SvkND" ))("") --// BABFT //--
+end
